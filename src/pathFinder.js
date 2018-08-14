ff2bc9f26230f76dc76ce502b3d88d6fe31bbc13
Elcoosp <elcoosp@gmail.com>
2018-08-14 16:45:52 +0200

    fix(pathFinder): See previous wrong commit

--- src/pathFinder.js
+++ src/pathFinder.js
@@ -1,20 +1,30 @@
 const isGoal = (a, b) => a[0] === b[0] && a[1] === b[1]
 const isOutOfBounds = (coords, maze) => coords.some(x => x > maze.length - 1)
-const isNotOpen = ([x, y], maze) => maze[x][y] !== '.'
+const isWall = ([x, y], maze) => maze[x] && maze[x][y] !== '.'
 
+const go = {
+  north: ([x, y]) => [x, y - 1],
+  east: ([x, y]) => [x + 1, y],
+  south: ([x, y]) => [x, y + 1],
+  west: ([x, y]) => [x - 1, y]
+}
 const pathFinder = (
   maze,
   coords = [0, 0],
   solution = [],
   goal = [maze.length - 1, maze.length - 1]
 ) => {
-  // Outside of maze bounds
   if (isOutOfBounds(coords, maze)) return false
-  // We reached the goal
   if (isGoal(coords, goal)) return true
-  // Current position is not valid
-  if (isNotOpen(coords, maze)) return false
+  if (isWall(coords, maze)) return false
   else {
     // Mark coords as part of the solution
+    const neighboors = ['north', 'east', 'south', 'west'].map(direction =>
+      pathFinder(maze, go[direction](coords))
+    )
   }
 }
+
+pathFinder(`.W.
+.W.
+W..`)
