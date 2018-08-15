f4f1f7fb36968b3c0be9faf393a4c18982092966
Elcoosp <elcoosp@gmail.com>
2018-08-15 09:54:58 +0200

    refactor(pathFinder): Correct isOutOfBounds and isWall

--- src/pathFinder.js
+++ src/pathFinder.js
@@ -1,6 +1,7 @@
 const isGoal = (a, b) => a[0] === b[0] && a[1] === b[1]
-const isOutOfBounds = (coords, maze) => coords.some(x => x > maze.length - 1)
-const isWall = ([x, y], maze) => maze[x] && maze[x][y] !== '.'
+const isOutOfBounds = (coords, maze) =>
+  coords.some(x => x > maze.length - 1 || x < 0)
+const isWall = ([x, y], maze) => maze[x] && maze[x][y] === 'W'
 
 const go = {
   north: ([x, y]) => [x, y - 1],
@@ -8,23 +9,26 @@ const go = {
   south: ([x, y]) => [x, y + 1],
   west: ([x, y]) => [x - 1, y]
 }
+
 const pathFinder = (
   maze,
   coords = [0, 0],
-  solution = [],
   goal = [maze.length - 1, maze.length - 1]
 ) => {
-  if (isOutOfBounds(coords, maze)) return false
   if (isGoal(coords, goal)) return true
+  if (isOutOfBounds(coords, maze)) return false
   if (isWall(coords, maze)) return false
   else {
     // Mark coords as part of the solution
-    const neighboors = ['north', 'east', 'south', 'west'].map(direction =>
-      pathFinder(maze, go[direction](coords))
-    )
+    for (const direction of ['north', 'east', 'south', 'west'])
+      if (pathFinder(maze, go[direction](coords))) return true
+    return false
   }
 }
 
-pathFinder(`.W.
+console.log(
+  pathFinder(`
+  .W.
 .W.
-W..`)
+...`)
+)
