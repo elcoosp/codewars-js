f6c1c2f6f32422889c39db28a119a637fd97761c
Elcoosp <elcoosp@gmail.com>
2018-08-16 12:35:50 +0200

    refactor(pathFinder): Add isExplored and addExplored functions

--- src/pathFinder.js
+++ src/pathFinder.js
@@ -2,6 +2,17 @@ const isGoal = (a, b) => a[0] === b[0] && a[1] === b[1]
 const isOutOfBounds = (coords, maze) =>
   coords.some(x => x > maze.length - 1 || x < 0)
 const isWall = ([x, y], maze) => maze[x] && maze[x][y] === 'W'
+const isExplored = (coords, explored) =>
+  explored[coords[0]] &&
+  (explored[coords[0]][coords[1]] === true ||
+    explored[coords[0]][coords[1]] === false)
+
+const addExplored = (coords, explored) => (
+  explored[coords[0]]
+    ? (explored[coords[0]][coords[1]] = true)
+    : (explored[coords[0]] = []) && (explored[coords[0]][coords[1]] = true),
+  explored
+)
 
 const go = {
   north: ([x, y]) => [x, y - 1],
@@ -13,22 +24,26 @@ const go = {
 const pathFinder = (
   maze,
   coords = [0, 0],
+  explored = [[]],
   goal = [maze.length - 1, maze.length - 1]
 ) => {
-  if (isGoal(coords, goal)) return true
+  console.log(coords)
+
+  if (isExplored(coords, explored)) return explored[coords[0]][coords[1]]
   if (isOutOfBounds(coords, maze)) return false
   if (isWall(coords, maze)) return false
+  if (isGoal(coords, goal)) return true
   else {
     // Mark coords as part of the solution
     for (const direction of ['north', 'east', 'south', 'west'])
-      if (pathFinder(maze, go[direction](coords))) return true
+      if (pathFinder(maze, go[direction](coords), explored)) return true
+
     return false
   }
 }
 
 console.log(
-  pathFinder(`
+  pathFinder(`.W.
   .W.
-.W.
-...`)
+  W..`)
 )
