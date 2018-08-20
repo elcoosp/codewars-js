2fb470709f8879bc24417f3d0d2afad4c9d14986
Elcoosp <elcoosp@gmail.com>
2018-08-20 10:05:57 +0200

    refactor(sumStrings): Remove useles index from generator, and zeroPad function. Concatenate the resu

--- src/sumStrings.js
+++ src/sumStrings.js
@@ -2,31 +2,20 @@
 // And the values at each array ends
 function* browseArraysByEnd(...arrs) {
   const xs = arrs.map(a => a.slice())
-  let index = 0
-  while (xs.some(a => a.length))
-    yield { values: xs.map(a => a.pop()), index }, index++
+  while (xs.some(a => a.length)) yield xs.map(a => a.pop())
 }
-
-// Add zerosNum 0 to a number
-const zeroPad = (zerosNum, n) =>
-  zeros === 0 ? n : +(n.toString() + '0'.repeat(zeros))
 
 const sumStrings = (a, b) => {
   let detention = 0,
-    result = 0
-  for (const { values, index } of browseArraysByEnd(
-    ...[a, b].map(s => s.split(''))
-  )) {
-    console.log(values)
+    result = ''
+  for (const nums of browseArraysByEnd(...[a, b].map(s => s.split('')))) {
+    const addition = nums.reduce((x, y) => x + (y ? +y : 0), 0) + detention
 
-    // Add while adding zeros depedning on current index
-    const addition = values.reduce(
-      (x, y) => zeroPad(index, x) + zeroPad(index, +y),
-      0
-    )
-    // TODO: make 10 depending on index
-    if (addition > 10) result += addition - 10
-    else result += addition
+    if (addition > 10)
+      (result = (addition - 10).toString() + result),
+        (detention = addition - 10)
+    else (result = addition.toString() + result), (detention = 0)
   }
   return result
 }
+console.log(sumStrings('99', '1'))
