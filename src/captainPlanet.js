ad1e05c63b9dd169a4b30309a3e2f3a8989c3f3d
Elcoosp <elcoosp@gmail.com>
2018-08-24 13:46:47 +0200

    refactor(Refactor toHighSentence to account for a high level of particle in multiple locations):

--- src/captainPlanet.js
+++ src/captainPlanet.js
@@ -2,7 +2,7 @@ const dataFile = `
 ##################################
 Location: DEU
 ##################################
-Ammonia: 023 particles
+Ammonia: 1023 particles
 Nitrogen Oxide: 019 particles
 Carbon Monoxide: 127 particles
 ##################################
@@ -28,6 +28,8 @@ const replaceAll = (splitter, joiner = '') => x =>
   x.split(splitter).join(joiner)
 const isLast = (i, a) => i === a.length - 1
 const splitNewLine = x => x.split('\n')
+const allIndexesOf = x => arr =>
+  arr.reduce((acc, v, i) => (v === x && acc.push(i), acc), [])
 
 // Cut the data in grouped sections
 const splitBySeparator = data => {
@@ -67,15 +69,28 @@ const maxProp = (
   [[key, value]] = Object.entries(o).filter(([k, value]) => value === max)
 ) => key
 
-const toSentence = data =>
-  data
+const toSentence = data => {
+  const toHighParticlesLocationIndexes = data
+    .map(({ Location, ...particleLevels }) => maxProp(particleLevels))
+    .map((particle, _, a) => ({ [particle]: allIndexesOf(particle)(a) }))
+
+  return toHighParticlesLocationIndexes
     .reduce((acc, locationData) => {
-      const { Location, ...particles } = locationData
+      const [[substance, locationIndexes]] = Object.entries(locationData)
 
-      return acc + toHighSentence(maxProp(particles), Location) + '. '
-    }, '')
+      const sentence = toHighSentence(
+        substance,
+        locationIndexes
+          .map(i => data[i].Location)
+          .join(', ')
+          .trim()
+      )
+      !acc.includes(sentence) && acc.push(sentence)
+      return acc
+    }, [])
+    .join('. ')
     .trim()
-
+}
 const parseData = () => {
   const locationsDataArr = splitBySeparator(dataFile).map(
     pipe(
