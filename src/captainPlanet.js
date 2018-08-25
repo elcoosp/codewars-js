138f63b1e4dd3c170bbf69e5931f1c28de6e8982
Elcoosp <elcoosp@gmail.com>
2018-08-25 13:28:53 +0200

    feat(captainPlanet): Finished version

--- src/captainPlanet.js
+++ src/captainPlanet.js
@@ -16,7 +16,7 @@ Carbon Monoxide: 120 particles
 ##################################
 Location: AUS
 ##################################
-Ammonia: 122 particles
+Ammonia: 1023 particles
 Nitrogen Oxide: 132 particles
 Carbon Monoxide: 099 particles
 ##################################
@@ -30,75 +30,77 @@ const isLast = (i, a) => i === a.length - 1
 const splitNewLine = x => x.split('\n')
 const allIndexesOf = x => arr =>
   arr.reduce((acc, v, i) => (v === x && acc.push(i), acc), [])
-
-// Cut the data in grouped sections
-const splitBySeparator = data => {
-  const SEPARATOR = '##################################'
-  return data
-    .split((SEPARATOR + '\n').repeat(2))
-    .map(
-      (s, i, a) =>
-        isLast(i, a)
-          ? replaceAll('\n' + SEPARATOR)(replaceAll(SEPARATOR + '\n')(s))
-          : replaceAll(SEPARATOR + '\n')(s)
-    )
-}
 // Transform an array of fields string (key/value separated by a comma) into an array of objects
-const fieldsToObj = (fieldKeyPredicate, fieldValueMapper) => fieldsArr =>
+const fieldsToObj = (
+  fieldKeyPredicate,
+  fieldValueMapper,
+  splitter = ':'
+) => fieldsArr =>
   fieldsArr.reduce((acc, field) => {
     if (field) {
-      const [key, value] = field.split(':').map(s => s.trim())
+      const [key, value] = field.split(splitter).map(s => s.trim())
       if (fieldKeyPredicate(key)) acc[key] = fieldValueMapper(value)
     }
     return acc
   }, {})
 
-// Keys which are allowed in the objectified data
-const isAllowedKey = k =>
-  ['Location', 'Ammonia', 'Nitrogen Oxide', 'Carbon Monoxide'].includes(k)
+const PARTICLES = ['Ammonia', 'Nitrogen Oxide', 'Carbon Monoxide']
 
-// Parse Int if particle field
+// Cut the data in sections
+const splitBySeparator = (
+  data,
+  separator = '##################################'
+) =>
+  data
+    .split((separator + '\n').repeat(2))
+    .map(
+      (s, i, a) =>
+        isLast(i, a)
+          ? replaceAll('\n' + separator)(replaceAll(separator + '\n')(s))
+          : replaceAll(separator + '\n')(s)
+    )
+
+// Keys which are allowed in the objectified data
+const isAllowedKey = k => ['Location', ...PARTICLES].includes(k)
+
 const sanitizeParticles = x => (x.includes('particles') ? parseInt(x, 10) : x)
 
 const toHighSentence = (substance, location) =>
   `${substance} levels in ${location} are too high`
 
-const maxProp = (
-  o,
-  max = Math.max(...Object.values(o)),
-  [[key, value]] = Object.entries(o).filter(([k, value]) => value === max)
-) => key
+const getMeasures = locationsDataArr => particleName =>
+  locationsDataArr.map(location => location[particleName])
 
-const toSentence = data => {
-  const toHighParticlesLocationIndexes = data
-    .map(({ Location, ...particleLevels }) => maxProp(particleLevels))
-    .map((particle, _, a) => ({ [particle]: allIndexesOf(particle)(a) }))
+const keepHighest = measures =>
+  measures.map(x => (x === Math.max(...measures) ? x : undefined))
 
-  return toHighParticlesLocationIndexes
-    .reduce((acc, locationData) => {
-      const [[substance, locationIndexes]] = Object.entries(locationData)
-
-      const sentence = toHighSentence(
-        substance,
-        locationIndexes
-          .map(i => data[i].Location)
-          .join(', ')
-          .trim()
-      )
-      !acc.includes(sentence) && acc.push(sentence)
-      return acc
-    }, [])
-    .join('. ')
-    .trim()
-}
-const parseData = () => {
-  const locationsDataArr = splitBySeparator(dataFile).map(
-    pipe(
-      splitNewLine,
-      fieldsToObj(isAllowedKey, sanitizeParticles)
-    )
+const highestMeasuresToLocations = locationsDataArr => highestMeasures =>
+  highestMeasures.reduce(
+    (acc, measure, i) =>
+      measure ? (acc.push(locationsDataArr[i].Location), acc) : acc,
+    []
   )
 
-  return toSentence(locationsDataArr)
-}
-console.log(parseData())
+const particleSentence = (acc, picLocations, i) =>
+  (acc += toHighSentence(PARTICLES[i], picLocations.join(', ')) + '. ')
+
+const toSentence = locationsDataArr =>
+  PARTICLES.map(
+    pipe(
+      getMeasures(locationsDataArr),
+      keepHighest,
+      highestMeasuresToLocations(locationsDataArr)
+    )
+  )
+    .reduce(particleSentence, '')
+    .trim()
+
+const parseData = () =>
+  toSentence(
+    splitBySeparator(dataFile).map(
+      pipe(
+        splitNewLine,
+        fieldsToObj(isAllowedKey, sanitizeParticles)
+      )
+    )
+  )
